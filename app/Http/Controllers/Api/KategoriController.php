c75c52d17d900216da79d5d5bb13ebb8d7f5160d
root <root@ivory.top>
2024-06-18 09:48:44 +0700

    [invent-05] Update Salah Satu Kategori

--- app/Http/Controllers/Api/KategoriController.php
+++ app/Http/Controllers/Api/KategoriController.php
@@ -56,9 +56,25 @@ class KategoriController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Kategori $kategori)
+    public function update(Request $request, string $id)
     {
-        //
+        $kategori = Kategori::find($id);
+
+        $request->validate([
+            'deskripsi'   => 'required',
+            'kategori'    => 'required',
+        ]);
+        
+        if (!$kategori) {
+            return response()->json(['status' => 'Kategori tidak ditemukan'], 404);
+        }else{
+            $kategori->update([
+                'deskripsi'=>$request->deskripsi,
+                'kategori'=>$request->kategori,
+            ]);
+
+        return response()->json(['status' => 'Kategori berhasil diubah'], 200);          
+        }
     }
 
     /**
